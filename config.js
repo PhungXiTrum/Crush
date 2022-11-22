35d9b8efb9989b209a7c88afc1558d8311b7f1d4
Nguyễn Nhật Quang <117562396+PhungXiTrum@users.noreply.github.com>
2022-11-22 14:54:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const CONFIG = {
     introDesc: `Em có đôi điều muốn nói với chị.
     Chị nhớ trả lời cho em nha`,
     btnIntro: 'Okeee',
-    title: 'Chị Quy ơi em thích chị ',
+    title: 'Chị Quy ơi em thích chị rất nhiều >3 ',
     desc: 'Em không biết hiện giờ chị thấy sao về em nhưng em hy vọng mình sẽ là người nắm tay và được bên cạnh chị trong những ngày về sau',
     btnYes: 'Chị đồng ý <33',
     btnNo: 'Hong đâu pé ơiii',
     question:'Chị cho em 1 chút cảm nhận của chị về em được không',
     btnReply: 'Send',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em muốn cho chị biết rằng khi đã tới bước này là em thực sự muốn nghiêm túc được theo đuổi chị',
+    mess: 'Em muốn cho chị biết rằng khi đã tới bước này là em thực sự nghiêm túc muốn theo đuổi chị',
     messDesc: 'Nếu như tối nay chị không bận gì thì em hẹn chị đi chơi nhé',
     btnAccept: 'Okiii lun <3',
     messLink: 'https://www.facebook.com/n.quang161/' //link mess của các bạn. VD: https://m.me/nam.nodemy
